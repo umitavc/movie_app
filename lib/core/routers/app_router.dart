ef20e7faa38d4b3bdd28b0d3409a3cc59180fcd8
umitavc <umitavc62@gmail.com>
2023-11-28 13:35:43 +0300

    app router added

---
+++ lib/core/routers/app_router.dart
@@ -0,0 +1,20 @@
+
+
+
+// import 'package:auto_route/auto_route.dart';
+// import 'package:movie_app/data/models/movies_model.dart';
+// import 'package:movie_app/features/home_screen/home_screen.dart';
+// import 'package:movie_app/features/home_screen/movies_detail_screen.dart';
+
+// part 'app_router.gr.dart';            
+              
+// @AutoRouterConfig()      
+// class AppRouter extends _$AppRouter {      
+    
+//   @override      
+//   List<AutoRoute> get routes => [      
+        
+//     AutoRoute(page: HomeRoute.page, initial: true),      
+//     AutoRoute(page: MoviesDetailRoute.page),
+//    ];  
+//  }
